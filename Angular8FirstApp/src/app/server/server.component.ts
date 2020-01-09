91a1a64f014c2be7b2aae269381aa6268a51980e
Wassim Ajjali <wassim.ajjali@lgesa.local>
2020-01-09 13:40:50 +0100

    String interpolation exemple

--- Angular8FirstApp/src/app/server/server.component.ts
+++ Angular8FirstApp/src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
-
+    serverId: number = 10;
+    serverStatus: String = 'isOffline';
+    getServerStatus()
+    {
+        return this.serverStatus;
+    }
 }
